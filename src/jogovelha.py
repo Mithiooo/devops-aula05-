4a2c2ff93f73479dc128481c6fa8023cfb7dc4bb
Mithiooo <38835388+Mithiooo@users.noreply.github.com>
2018-09-04 21:51:30 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,11 +3,11 @@ def inicializar():
 	for i in range(3):
 		linha = []
 		for j in range(3):
-			linha.appen("X")
+			linha.append("X")
 			tab.append(linha)
 	return tab
 def main():
 	jogo = inicializar()
 	print(jogo)
-	if __name__ == "__main__":
-main()
+if __name__ == "__main__":
+	main()
